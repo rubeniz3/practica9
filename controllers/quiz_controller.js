74ffbfef2eca175a754092248233d8561cabb790
Ruben <ruben-i-f@hotmail.com>
2016-04-27 17:12:43 +0100

    Añadida página de créditos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,3 +14,8 @@ exports.check = function(req,res,next){
 	res.render('quizzes/result', {result: result,
 				      answer: answer});
 };
+
+//GET /author
+exports.author = function(req,res,next){
+	res.render('author');
+};
