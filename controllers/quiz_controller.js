16a1ae2995f3cad902979a54074883cf08a27545
Ruben <ruben-i-f@hotmail.com>
2016-05-03 19:39:23 +0100

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,40 +1,48 @@
 var models = require('../models');
 
-//GET /question
-exports.question = function(req,res,next){
+// GET /quizzes
+exports.index = function(req, res, next) {
   models
   .Quiz
-  .findOne() // Busca la primera pregunta
-  .then(function(quiz) {
-    if (quiz) {
-	var answer = req.query.answer || '';
-	res
-        .render('quizzes/question',{question: quiz.question, answer: answer});
-    }
-    else {
-      throw new Error('No hay preguntas en la BBDD.');
-    }
-  }).catch(function(error) { next(error);});
-};
-
-//GET /check
-exports.check = function(req,res,next){
-  models
-  .Quiz
-  .findOne() // Busca la primera pregunta
-  .then(function(quiz) {
-    if (quiz) {
-	var answer = req.query.answer || "";
-	var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result, answer: answer});
-    }
-    else {
-      throw new Error('No hay preguntas en la BBDD.');
-    }
-  }).catch(function(error) { next(error); });
+  .findAll()
+  .then(function(quizzes) {
+    res.render('quizzes/index.ejs', { quizzes: quizzes});
+  })
+  .catch(function(error) {next(error); });
 };
 
 //GET /author
 exports.author = function(req,res,next){
 	res.render('author');
+};
+
+
+
+//GET /quizzes/:id
+exports.show = function(req, res, next) {
+  models
+  .Quiz
+  .findById(req.params.quizId)
+  .then(function(quiz) {
+    if (quiz) {
+      var answer = req.query.answer || '';
+      res.render('quizzes/show', {quiz: quiz, answer: answer});
+    } else { throw new Error('No existe ese quiz en la BBDD.'); }
+  })
+  .catch(function(error) { next(error); });
+};
+
+//GET /quizzes:id/check
+exports.check = function(req, res) {
+  models
+  .Quiz
+  .findById(req.params.quizId)
+  .then(function(quiz) {
+    if (quiz) {
+      var answer = req.query.answer || "";
+      var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+      res.render('quizzes/result', {quiz: quiz, result: result, answer: answer });
+    } else { throw new Error('No existe ese quiz en la BBDD.'); }
+  })
+  .catch(function(error) {next(error); });
 };
--- models/index.js
+++ models/index.js
@@ -4,8 +4,24 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null,
-		      { dialect: "sqlite", storage: "quiz.sqlite" });
+//    DATABASE_URL = sqlite:///
+//    DATABASE_STORAGE = quiz.sqlite
+
+// Usar BBDD Postgres:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
+
+var url, storage;
+
+if (!process.env.DATABASE_URL) {
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
+} else {
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url, { storage: storage, omitNull: true
+                              });
 
 // Importar la definición de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
@@ -18,7 +34,9 @@ sequelize
     .then(function (c) {
       if (c === 0) { // la tabla se inicializa si está vacía
         return Quiz
-          .create({ question: 'Capital de Italia', answer: 'Roma' })
+          .bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
+			{question: 'Capital de Portugal', answer: 'Lisboa'}
+                     ])
           .then(function() {
 	    console.log('Base de datos inicializada con datos');
 	  });
