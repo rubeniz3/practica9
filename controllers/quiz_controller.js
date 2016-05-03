7ea88e36b9212446cc105000116ab8debf78bbe9
Ruben <ruben-i-f@hotmail.com>
2016-05-03 18:00:07 +0100

    Creación BBDD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,37 @@
+var models = require('../models');
+
 //GET /question
 exports.question = function(req,res,next){
+  models
+  .Quiz
+  .findOne() // Busca la primera pregunta
+  .then(function(quiz) {
+    if (quiz) {
 	var answer = req.query.answer || '';
-	res.render('quizzes/question',{question: 'Capital de Italia',
-				       answer: answer});
+	res
+        .render('quizzes/question',{question: quiz.question, answer: answer});
+    }
+    else {
+      throw new Error('No hay preguntas en la BBDD.');
+    }
+  }).catch(function(error) { next(error);});
 };
 
 //GET /check
 exports.check = function(req,res,next){
-
+  models
+  .Quiz
+  .findOne() // Busca la primera pregunta
+  .then(function(quiz) {
+    if (quiz) {
 	var answer = req.query.answer || "";
-
-	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result,
-				      answer: answer});
+	var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+	res.render('quizzes/result', {result: result, answer: answer});
+    }
+    else {
+      throw new Error('No hay preguntas en la BBDD.');
+    }
+  }).catch(function(error) { next(error); });
 };
 
 //GET /author
